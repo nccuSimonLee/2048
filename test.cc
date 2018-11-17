76faf7e7e61de546beef14e3bbc383c03c60d396
nccuSimonLee <simon2020291@gmail.com>
2018-11-17 15:03:21 +0800

    main file

--- test.cc
+++ test.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "ConsoleMenu.h"
 #include "Board.h"
+#include "BoardPresent.h"
 using namespace std;
 
 const char * const menu_items[]={"I = UP",
@@ -10,15 +11,18 @@ const char * const menu_items[]={"I = UP",
 
 int main() {
 	Board board(4);
+	BoardPresent presenter;
 	board.initBoard();
-	board.printBoard();
+	//board.printBoard();
+	presenter.print(board);
 	ConsoleMenu menu(4, menu_items);
 	int move;
 	while(board.getZeroPos() != -1) {
 		menu.print();
 		move = menu.getMove();
 		board.updateBoard(move);
-		board.printBoard();
+		//board.printBoard();
+		presenter.print(board);
 	}
 	return 0;
 }
