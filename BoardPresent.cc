930aaa7f2e4f716f0f61865863ccb081d67c8d00
nccuSimonLee <simon2020291@gmail.com>
2018-11-17 15:45:10 +0800

    [change] the screen only shows the current board now.

--- BoardPresent.cc
+++ BoardPresent.cc
@@ -1,22 +1,26 @@
 #include "Board.h"
 #include "BoardPresent.h"
-#include <cstdio>
+#include <iostream>
+#include <string>
+using namespace std;
 
 BoardPresent::BoardPresent(int bound_size) {
 	board_bound_size = bound_size;
 }
 
 
-void BoardPresent::print(const Board &board){
+
+void BoardPresent::printBoard(const Board &board){
 	// Your code goes here
 	// 從上往下一列一列?
+	cout << string(50, '\n');
 	for (int i = 0; i < board.size; i++) { // row
 		printBound(); // 上?界
 		printInside(board); // 從這開始描繪棋格
 		for (int j = 0; j < board.size; j++) { // column
 			printNumber(board.elements[i][j], board);
 		}
-		printf("*\n");
+		cout << "*\n";
 		printInside(board);// 棋格描繪完?
 	}
 	printBound(); // 下?界
@@ -24,19 +28,19 @@ void BoardPresent::print(const Board &board){
 
 void BoardPresent::printBound() {
 	for (int i = 0; i < board_bound_size; i++)
-		printf("*");
-	printf("\n");
+		cout << "*";
+	cout << "\n";
 }
 
 void BoardPresent::printInside(const Board &board){
 	// ?出棋盤內部結?
 	int n_space = (board_bound_size - board.size + 1) / board.size; // 棋盤每格裡面的空白數量
 	for (int i = 0; i < board.size; i++) { // column
-		printf("*");
+		cout << "*";
 		for (int j = 0; j < n_space; j++)
-			printf(" ");
+			cout << " ";
 	}
-	printf("*\n");
+	cout << "*\n";
 }
 
 void BoardPresent::printNumber(int number, const Board &board){
@@ -47,16 +51,16 @@ void BoardPresent::printNumber(int number, const Board &board){
 		n_len = nDigits(number);
 		if (n_len >= 4) inside_spa = 1;
 		else inside_spa = n_space / 2 - n_len + 1;
-		printf("*");
+		cout << "*";
 		for (j = 0; j < inside_spa; j++)
-			printf(" ");
-		printf("%d", number);
+			cout << " ";
+		cout << number;
 		for (j = 0; j < n_space - (n_len + inside_spa); j++)
-			printf(" ");
+			cout << " ";
 	}
 	else {
-		printf("*");
+		cout << "*";
 		for (j = 0; j < n_space; j++)
-			printf(" ");
+			cout << " ";
 	}
 }
--- BoardPresent.h
+++ BoardPresent.h
@@ -8,13 +8,14 @@ class Board;
 class BoardPresent {
 	public:
 		BoardPresent(int bound_size=29);
-		void print(const Board &board);
+		void printBoard(const Board &board);
 		
 	private:
 		int board_bound_size;
 		void printBound();
 		void printInside(const Board &board);
 		void printNumber(int number, const Board &board);
+		
 };
 
 
